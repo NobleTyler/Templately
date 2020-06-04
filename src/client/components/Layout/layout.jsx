f4d01798c293babd39db0c9385f3b96ccd35be5a
Tyler Noble <tylersnoble@gmail.com>
2020-06-03 22:40:35 -0400

    Made some layout stuff. Doesnt compile tho

--- src/client/components/Layout/layout.jsx
+++ src/client/components/Layout/layout.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Classes from './layout.css'
-import ContactForm from '../contactForm/contactform'
-import Output from '../Output/output'
+import React from 'react';
+import Classes from './layout.css';
+import ContactForm from '../contactForm/contactform';
+import Output from '../Output/output';
 
-const layout = props => {
-    return(
-        <React.Fragment>
-            
-        </React.Fragment>
-    )
-    
-}
+const layout = props => (
+ return(
+      <React.Fragment>
+    <ContactForm />
+    <Output
+      show={props.generated}
+    />
+  </React.Fragment>
+)
+);
 
-export default layout
+export default layout;
