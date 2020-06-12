9073403d25978cfc8050d98351dbd93168cf2e11
Tyler Noble <tylersnoble@gmail.com>
2020-06-12 18:12:17 -0400

    Make more json

--- src/server/index.js
+++ src/server/index.js
@@ -10,9 +10,11 @@ fs.readFile(`${__dirname}/greetings.json`, (err, data) => {
   console.log(greetings);
 });
 
+const message = 'Something is wrong. Please fix this app.';
+
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.get('/api/getGreeting', (req, res) => res.send({ message: 'This works eh?' }));
+app.get('/api/getGreeting', (req, res) => res.send({ message }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
